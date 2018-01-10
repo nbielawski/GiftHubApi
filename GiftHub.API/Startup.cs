bea8c70bc8dbb287e3f5ed794a32e2ce1d4fa071
digitopoi <hubo85@gmail.com>
2018-01-10 09:22:13 -0600

    add roles on startup

--- GiftHub.API/Startup.cs
+++ GiftHub.API/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using GiftHub.API.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
 
@@ -14,6 +17,35 @@ namespace GiftHub.API
         {
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@gifthub.com";
+                user.Email = "admin@gifthub.com";
+
+                string userPassword = "gifthubadmin";
+
+                var chkUser = userManager.Create(user, userPassword);
+
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "admin");
+                }
+            }
         }
     }
 }
