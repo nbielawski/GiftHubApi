c59a5e000baaed5110a0d91e18118f4d1ef74e62
digitopoi <hubo85@gmail.com>
2018-01-16 10:28:52 -0600

    Add manage users endpoint

---
+++ GiftHub.API/Controllers/ManageUsersController.cs
@@ -0,0 +1,26 @@
+﻿using GiftHub.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GiftHub.API.Controllers
+{
+    public class ManageUsersController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var service = ManageUsersService();
+            return Ok(service.GetUsers());
+        }
+
+        private ManageUsers ManageUsersService()
+        {
+            var service = new ManageUsers();
+            return service;
+        }
+      
+    }
+}
