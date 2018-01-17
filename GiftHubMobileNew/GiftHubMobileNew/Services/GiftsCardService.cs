c6c2ba5a5e16e945f2a62f569a89fc4d07fc522d
Nick Bielawski <nbielawski12@gmail.com>
2018-01-17 10:33:24 -0500

    changed web config for deployment

---
+++ GiftHubMobileNew/GiftHubMobileNew/Services/GiftsCardService.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+using GiftHubMobileNew.Models;
+
+namespace GiftHubMobileNew.Services
+{
+    internal class GiftsCardService
+    {
+        public static string BearerToken { get; set; }
+        private const string _apiUrl = "http://localhost:50203";
+
+      
+        public async Task<bool> Login(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var url = $"{_apiUrl}/token";
+
+                 
+                    var requestString = $"grant_type=password&username={HttpUtility.UrlEncode(username.Trim())}&password={HttpUtility.UrlEncode(password.Trim())}";
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "www-form-urlencoded; charset=utf-8");
+
+                   
+                    var result = await client.PostAsync(url, new StringContent(requestString));
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        BearerToken = null;
+                        return false;
+                    }
+
+                    var stringResponseFromServer = await result.Content.ReadAsStringAsync();
+                    var response = JsonConvert.DeserializeObject<OauthBearerTokenResponse>(stringResponseFromServer);
+                    if (response == null) return false;
+                    BearerToken = response.access_token; 
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
